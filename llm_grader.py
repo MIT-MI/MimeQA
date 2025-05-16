c76d1a4808f3549345b1c32e2dac8c33e2c73a37
MeganTj <supermeg100@gmail.com>
2025-05-16 07:02:30 -0400

    Update LLM grader

--- llm_grader.py
+++ llm_grader.py
@@ -1,3 +1,4 @@
+# Grade LLM with a human baseline
 import argparse
 import os
 import openai
@@ -15,20 +16,21 @@ class Evaluation(BaseModel):
     explanation: str
     
 openai.api_key = os.getenv("MIT_OPENAI_API_KEY")
+
     
 GRADE_INSTRUCTION = """
-Answer Equivalence Instructions:
+Answer Grading Instructions:
 Carefully consider the following question and answers regarding understanding of a mime performance.
-You will be shown a few "gold-standard" answers from human annotators, referred to as the "Reference Answers", and a "Candidate Answer".
-Your task is to determine whether the candidate answer is semantically equivalent to either of the reference answers.
-In general, a candidate answer is a good answer in place of the "gold" reference if all of the following are satisfied:
+You will be shown a "gold-standard" answer from a human annotator, referred to as the "Reference Answer", and a "Candidate Answer".
+Your task is to determine whether the candidate answer is a good answer in place of the "gold" reference using the following criteria:
 
 1. The candidate directly answers the question without deviation or misunderstanding.
-2. The candidate contains all relevant information as the reference, taking into account the question; in particular it does not omit any relevant information present in the reference.
-3. The candidate contains neither misleading or excessive superfluous information not present in any of the reference answers, taking into account the question; ; in particular it does not hallucinate story plots not present in the reference.
-4. Since the videos are mime performances, invisible actions, objects, or the mime actor portraying objects should be considered correct if and only if they are relevant to the question.
+2. The candidate does not contain misleading information and does not hallucinate story plots not present in the reference answer.
+3. Since the videos are mime performances, invisible actions, objects, or the mime actor portraying objects should be considered correct if and only if they are relevant to the question.
+4. The candidate answer can be a good answer in place of the reference answer even if they are not semantically equivalent, as long as they are 
+in the same ballpark, given that there can be multiple interpretations of a mime acting out invisible actions or objects.
 
-Your response should be one word, "TRUE" or "FALSE", and a brief explanation of your decision. You should respond "TRUE" if the candidate is a good answer in place of any of the reference answers, and "FALSE" otherwise.
+Your response should be one word, "TRUE" or "FALSE", and a brief explanation of your decision. You should respond "TRUE" if the candidate is a good answer in place of the reference answer, and "FALSE" otherwise.
 """
 
 GRADE_PROMPT = """
@@ -36,29 +38,27 @@ Question:
 "{question}"
 Candidate Answer:
 "{candidate_answer}"
-Reference Answer 1:
-"{ref_answer_1}"
-Reference Answer 2:
-"{ref_answer_2}
+Reference Answer:
+"{ref_answer}"
 Equivalent? (True/False, and why?):
 """
     
-async def check_answer(question: str, candidate_answer: str, reference_answer_1: str, reference_answer_2: str) -> Evaluation:
+async def check_answer(question: str, candidate_answer: str, reference_answer: str) -> Evaluation:
     grader = LLM(llm_str = "gpt-4o", default_instructions=GRADE_INSTRUCTION) #gpt4o
-    prompt = GRADE_PROMPT.format(question=question, candidate_answer=candidate_answer, ref_answer_1=reference_answer_1, ref_answer_2=reference_answer_2)
+    prompt = GRADE_PROMPT.format(question=question, candidate_answer=candidate_answer, ref_answer=reference_answer,)
     message = create_message("user", prompt)
     response = await grader.parse_completion([message], response_format=Evaluation)
     return response
 
 # iterate through each row and evaluate the candidate answer
-async def evaluate_worker(semaphore, question: str, candidate_answer: str, reference_answer_1: str, reference_answer_2: str):
+async def evaluate_worker(semaphore, question: str, candidate_answer: str, reference_answer):
     async with semaphore:
-        response = await check_answer(question, candidate_answer, reference_answer_1, reference_answer_2)
+        response = await check_answer(question, candidate_answer, reference_answer)
         evaluation = response.choices[0].message.parsed
         await asyncio.sleep(2)
         return evaluation
 
-async def evaluate_all(df):
+async def evaluate_all(reference_df, df):
     max_workers = 10
     semaphore = asyncio.Semaphore(max_workers)
     tasks = []
@@ -66,11 +66,8 @@ async def evaluate_all(df):
     for idx, row in df.iterrows():
         question = row["augmented_question"]
         candidate_answer = row["generated_response"]
-        reference_answer_1 = row["answer_1"]
-        reference_answer_2 = row["answer_2"]
-        if pd.isnull(candidate_answer):
-            continue
-        tasks.append(evaluate_worker(semaphore, question, candidate_answer, reference_answer_1, reference_answer_2))
+        reference_answer = reference_df.loc[idx, "reference_answer"]
+        tasks.append(evaluate_worker(semaphore, question, candidate_answer, reference_answer))
         indices.append(idx)
     
     results = await tqdm.gather(*tasks, desc=f"Evaluating questions")
@@ -93,9 +90,11 @@ if __name__ == "__main__":
     args = parse_args()
     root = Path(__file__).parent.parent
     data_dir = root / "data"
-
+    reference_df = pd.read_csv(data_dir / f"annotation_final_human.csv")
     df = pd.read_csv(data_dir / f"{args.results_pref}_annotation_generated.csv")
     df["Auto Eval"] = None
     df["Auto Eval Explanation"] = None
-    asyncio.run(evaluate_all(df))
-    df.to_csv(data_dir / f"{args.results_pref}_annotation_auto_eval_evaluated.csv", index=False)
+    asyncio.run(evaluate_all(reference_df, df))
+    output_dir = root / "data_human"
+    os.makedirs(output_dir, exist_ok=True)
+    df.to_csv(output_dir / f"{args.results_pref}_annotation_auto_eval_evaluated.csv", index=False)
